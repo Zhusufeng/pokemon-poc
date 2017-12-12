9ca1994db1850770457af38de601ad769fe9c1f4
Lisa Gee <zhusufeng@gmail.com>
2017-12-11 18:54:07 -0800

    Import PokemonsService to index.js

--- app/pokemons/index.js
+++ app/pokemons/index.js
@@ -3,8 +3,10 @@ import uirouter from 'angular-ui-router';
 
 import routes from './pokemons.routes.js';
 import pokemonList from './pokemon-list/pokemon-list.component';
+import PokemonsService from './pokemons.service'
 
 export default angular.module('pokemonPoc.pokemons', [uirouter])
   .config(routes)
   .component('pokemonList', pokemonList)
+  .service('PokemonsService', PokemonsService)
   .name
