8a3a8e664bd77eb7b8401a3c19d3873dec729218
Lisa Gee <zhusufeng@gmail.com>
2017-12-11 18:47:40 -0800

    Update pokemonPoc.pokemons module to get pokemonList

--- app/pokemons/index.js
+++ app/pokemons/index.js
@@ -2,6 +2,9 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
 import routes from './pokemons.routes.js';
+import pokemonList from './pokemon-list/pokemon-list.component';
 
 export default angular.module('pokemonPoc.pokemons', [uirouter])
   .config(routes)
+  .component('pokemonList', pokemonList)
+  .name
