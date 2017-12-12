97d0491aa908ce8d7a821b1ebc90a275a1f72e60
Lisa Gee <zhusufeng@gmail.com>
2017-12-11 18:37:23 -0800

    Import pokemons module to main module

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
 import routes from './app.routes';
+import pokemons from './pokemons'
 
-angular.module('pokemonPoc', [uirouter])
+angular.module('pokemonPoc', [uirouter, pokemons])
   .config(routes)
